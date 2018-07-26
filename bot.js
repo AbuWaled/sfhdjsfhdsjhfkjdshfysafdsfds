8d4de7eca980b313e166924236a0a0666e1d1b0e
ahmadomaxherenow <41526951+AbuWaled@users.noreply.github.com>
2018-07-26 22:57:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '+'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$help | $inv ${client.guilds.size} Servers ${client.users.size} Users `,"https://www.twitch.tv/dggamingbot")
+client.user.setGame(`+help | +inv ${client.guilds.size} Servers  `,"https://www.twitch.tv/dggamingbot")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
