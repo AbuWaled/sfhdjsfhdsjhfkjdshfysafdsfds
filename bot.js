7d2cecbf7233b730d546b5eef7dcf306b7036858
ahmadomaxherenow <41526951+AbuWaled@users.noreply.github.com>
2018-07-26 22:56:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,19 +3,29 @@ const client = new Discord.Client();
 const prefix = '+'
 
 client.on('ready', () => {
-  console.log('༺✴✴✴✴✴✴✴✴✴✴✴✴✴✴✴✴✴✴༻');
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`$help | $inv ${client.guilds.size} Servers ${client.users.size} Users `,"https://www.twitch.tv/dggamingbot")
   console.log('')
-  console.log('            ༺✴✴✴✴✴✴✴✴✴✴✴✴✴✴✴✴✴✴༻')
-  console.log('              Bot Is Online')
-  console.log('            「▉▉▉▉▉▉▉▉▉▉▉」')
   console.log('')
-  console.log(`Name Bot Login ${client.user.tag}!`);
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
   console.log('')
-  console.log(`Bot In Servers [ " ${client.guilds.size} " ]`);
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
   console.log('')
-  console.log(`Bot Users Use [ " ${client.users.size} " ]`);
+  console.log('Informations :')
   console.log('')
-  console.log('༺✴✴✴✴✴✴✴✴✴✴✴✴✴✴✴✴✴✴༻')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
 
 
