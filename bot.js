5fa890e3a27846029042c0e48b63305176ff20cb
ahmadomaxherenow <41526951+iia7madg3meryt@users.noreply.github.com>
2018-07-31 17:54:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -254,13 +254,7 @@ client.on('guildCreate', guild => {
       guild.owner.send(embed)
 });
 
-client.on('voiceStateUpdate', (old, now) => {
-  const channel = client.channels.get('472361645910392833');
-  const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
-  const size = channel.name.match(/\[\s(\d+)\s\]/);
-  if (!size) return channel.setName(`Voice Online: ${currentSize}`);
-  if (currentSize !== size) channel.setName(`Voice Online: ${currentSize}`);
-});
+
 
 var AsciiTable = require('ascii-data-table').default
 client.on('message', message =>{
