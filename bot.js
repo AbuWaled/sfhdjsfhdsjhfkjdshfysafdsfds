f2553a7b300e2250f4a814dcfedd293c5dac0770
ahmadomaxherenow <41526951+JFHWYFHEDY9FR7923RYBHFKDSCF@users.noreply.github.com>
2018-11-25 20:28:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,6 @@ const Discord = require('discord.js');
 
 const Util = require('discord.js');
 
-const moment = require("moment");
-
 const getYoutubeID = require('get-youtube-id');
 
 const fetchVideoInfo = require('youtube-info');
