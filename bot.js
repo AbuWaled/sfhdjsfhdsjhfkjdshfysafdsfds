183e117639b2ea413b2509868a0ae8c64f460227
ahmadomaxherenow <41526951+JFHWYFHEDY9FR7923RYBHFKDSCF@users.noreply.github.com>
2019-09-01 14:19:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,54 +1,154 @@
 const Discord = require('discord.js');
 
-const Util = require('discord.js');
+const client = new Discord.Client();
 
-const getYoutubeID = require('get-youtube-id');
-
-const fetchVideoInfo = require('youtube-info');
-
-const YouTube = require('simple-youtube-api');
-
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-
-const queue = new Map();
-
-const ytdl = require('ytdl-core');
-
-const db = require('quick.db') // لا تنسى تحمل البكج ذا | npm i quick.db@7.0.0-b22 | او ضيف ذا السطر لملف البكج اذا كان خادمك مو وندز او لينكس : "quick.db": "^7.0.0-b22",
-
-const fs = require('fs');
-
-const client = new Discord.Client({disableEveryone: true});
+const fs = require("fs");
 
 const prefix = "+";
-/////////////////////////
-////////////////////////
+
+const adminprefix = "+"; 
 
 client.on('ready', () => {
-client.user.setStatus("dnd");
-  });
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log(' ID [ " ${user.id} "]')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+client.on('message', message => {	
+              if(!message.channel.guild) return;	
+    if(message.content.startsWith('+bc')) {	
+    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));	
+  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );	
+    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);	
+    let copy = "Turbo Bot";	
+    let request = `Requested By ${message.author.username}`;	
+    if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {	
+    msg.react('✅')	
+    .then(() => msg.react('❌'))	
+    .then(() =>msg.react('✅'))	
+
+
+
+     let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;	
+    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;	
+       let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });	
+    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });	
+    reaction1.on("collect", r => {	
+    message.channel.send(`☑ |   ${message.guild.members.size} يتم ارسال البرودكاست الى عضو `).then(m => m.delete(5000));	
+    message.guild.members.forEach(m => {	
+    var bc = new	
+       Discord.RichEmbed()	
+       .setColor('RANDOM')	
+       .setTitle('البرودكاست') .addField('السيرفر', message.guild.name) .addField('المرسل', message.author.username)	
+       .addField('الرساله', args)	
+       .setThumbnail(message.author.avatarURL)	
+       .setFooter(copy, client.user.avatarURL);	
+    m.send({ embed: bc })	
+    msg.delete();	
+    })	
+    })	
+    reaction2.on("collect", r => {	
+    message.channel.send(`**تم ايقاف ارسال البرودكاست.**`).then(m => m.delete(5000));	
+    msg.delete();	
+    })	
+    })	
+    }	
+    })
+
+
+//bc online
 
 client.on('message', message => {
-const myID = "444126346676011028";
-  if(!message.channel.guild) return;
+    if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('+Founder')){
-message.channel.sendMessage('جار ارسال الرسالة |✅')
+if (message.content.startsWith('+ownerbc')){
+if (message.author.id !== '444126346676011028') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+if(!message.author.id === '444126346676011028') return;
+message.channel.sendMessage('Now Sending |✅')
 client.users.forEach(m =>{
-var bc = new
-Discord.RichEmbed()
-.setColor('RANDOM')
-.setTitle('Broadcast')
-.addField('السيرفر:', message.guild.name)
-.addField('المرسل:', message.author.username)
-.addField('الرسالة:', args)
-m.send({ embed: bc })
+m.sendMessage(args)
 })
 }
 });
 
 
+  client.on("message", message => {   // Alpha Codes Ghost
+   
+              if (message.content.startsWith(prefix + "obc")) {    // Alpha Codes Ghost
+                           if (!message.member.hasPermission("ADMINISTRATOR"))  return;   // Alpha Codes Ghost
+    let args = message.content.split(" ").slice(1);  // Alpha Codes Ghost
+    var argresult = args.join(' '); 
+    message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {  // Alpha Codes Ghost
+   m.send(`${argresult}\n ${m}`);   // Alpha Codes Ghost
+  })
+   message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` :mailbox:  عدد المستلمين `); 
+   message.delete(); 
+  };     
+  });
+
+client.on('message', message => {
+    var  user = message.mentions.users.first() || message.author;  // Alpha Codes Ghost
+if (message.content.startsWith("#avatar")) {
+message.channel.send(`This avatar For ${user} link : ${user.avatarURL}`);
+}
+});
+
+client.on('ready',  () => {
+    console.log('تم تشغيل :Broadcast  ');
+    console.log(`Logged in as * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
+    console.log(`Logged in as * [ " ${client.user.username} " ] Users! [ " ${client.users.size} " ]`);
+    console.log(`Logged in as * [ " ${client.user.username} " ] channels! [ " ${client.channels.size} " ]`);
+  });
+
+
+  client.on("message", message => {
+  
+              if (message.content.startsWith(prefix + "offbc")) {
+                           if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+    let args = message.content.split(" ").slice(1);
+    var argresult = args.join(' '); 
+    message.guild.members.filter(m => m.presence.status !== 'online').forEach(m => {
+   m.send(`${argresult}\n ${m}`);
+  })
+   message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'Offline').size}\` :mailbox:  عدد المستلمين `); 
+   message.delete(); 
+  };     
+  });
 
 
 
-client.login(process.env.BOT_TOKEN);
+ 
+
+
+
+
+
+
+
+
+client.login(process.env.BOT);
